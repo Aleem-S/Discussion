6d9f71b88fced1da032d46efb6090f80fe70473f
Aleem-Sheikh <44214136+Aleem-Sheikh@users.noreply.github.com>
2020-11-16 00:21:10 +0000

    finished signin with email password

--- dexdiscuss/src/components/Login/SignIn.js
+++ dexdiscuss/src/components/Login/SignIn.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from '../FormInput/FormInput';
 import CustomButton from '../CustomButton/CustomButton';
-import {signInWithGoogle} from '../../firebase/firebase.util'
+import {auth, signInWithGoogle} from '../../firebase/firebase.util'
 
 import './SignIn'
 
@@ -16,10 +16,16 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-
-    this.setState({ email: '', password: '' });
+    const {email, password} = this.state;
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });  
+    }
+    catch(error)
+    {}
+    
   };
 
   handleChange = event => {
